fa35a289b3ca7f48ec0befd96808c5fa395d5a0f
Leo Bruell <leobruell@gmail.com>
2022-11-16 11:41:04 -0500

    Create add book form html

--- library.js
+++ library.js
@@ -1,4 +1,4 @@
-let myLibary = [new Book('My Life', 'Leo Bruell', '1,000', true), new Book('My Second Life', 'Leo Bruell', '1,0', false)]
+let myLibary = [new Book('My Life', 'Leo Bruell', '1000', true), new Book('My Second Life', 'Leo Bruell', '10', false)]
 
 function Book(title, author, pages, read){
     this.title = title
@@ -19,19 +19,63 @@ Book.prototype.info = function() {
     }
 }
 
-bookOne = new Book('My Life', 'Leo Bruell', '1,000', true)
-console.log(bookOne.info())
-
 function showBooks(){
     let bookList = document.querySelector('.book-list')
     for (book in myLibary){
         let bookDiv = document.createElement('div')
         bookDiv.classList.add('book-div')
         let bookIcon = document.createElement('p')
-        bookIcon.textContent = myLibary[book].title
+        bookIcon.innerHTML = 'Title: ' + myLibary[book].title + '<br />' + 'Author: ' + myLibary[book].author + '<br />' + 'Pages: ' + myLibary[book].pages
         bookDiv.appendChild(bookIcon)
         bookList.appendChild(bookDiv)
     }
 }
 showBooks()
 console.log(myLibary)
+
+function addForm(event){
+    let theForm = document.createElement('div')
+    theForm.classList.add('the-form')
+
+    theForm.innerHTML = 
+    `
+    <div class="the-form">
+    <div class="title">
+        <label for="title">Title:</label>
+        <input type="text" name="title" id="title" required>
+    </div>
+    <div class="author">
+        <label for="author">Author:</label>
+        <input type="text" name="author" id="author" required>
+    </div>
+    <div class="page-count">
+        <label for="pages">Number of Pages:</label>
+        <input type="number" name="page-count" id="pages" required>
+    </div>
+    <div class="read">
+        <label for="read">Have you read it?</label>
+        <div>
+            <div class="radio-button">
+                <label for="radio-yes">Yes</label>
+                <input type="radio" id="read-yes" name="read-it" value="yes">
+            </div>
+            <div class="radio-button">
+                <label for="radio-no">No</label>
+                <input type="radio" id="read-no" name="read-it" value="no">
+            </div>
+        </div>
+    </div>
+    <div class="submit-div">
+        <button class="submit-form" type="button">Add Book</button>
+    </div>
+    </div>
+    `
+    let formDiv = document.querySelector('.form-div')
+    formDiv.appendChild(theForm)
+
+    openForm.remove()
+    openForm.removeEventListener('click', addForm)
+}
+
+let openForm = document.querySelector('.open-form')
+openForm.addEventListener('click', addForm)
